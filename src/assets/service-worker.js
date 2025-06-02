bd9fd679f10032b02a94156aba227d6964655a6e
laucg00 <laucg00@hotmail.com>
2025-06-02 14:05:06 +0200

    Configuro pwa

--- src/assets/service-worker.js
+++ src/assets/service-worker.js
@@ -21,12 +21,12 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   const url = event.request.url;
 
-  // Cachear las respuestas de la API de Marvel 
+  // Si la solicitud es a la API de Marvel, almacenar la respuesta en caché
   if (url.includes('gateway.marvel.com')) {
     event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request).then(fetchResponse => {
-          return caches.open(API_CACHE_NAME).then(cache => {
+      caches.open('marvel-api-cache').then(cache => {
+        return cache.match(event.request).then(response => {
+          return response || fetch(event.request).then(fetchResponse => {
             cache.put(event.request, fetchResponse.clone());
             return fetchResponse;
           });
@@ -34,7 +34,7 @@ self.addEventListener('fetch', event => {
       })
     );
   } else {
-    // Para otros recursos estáticos, devolver desde caché si está disponible
+    // Para otros archivos estáticos, usar el caché normal
     event.respondWith(
       caches.match(event.request).then(response => {
         return response || fetch(event.request);
